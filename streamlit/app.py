2339b5331652c2f95f7ec62f569db29be0b51781
parvashah-create <jmparvashah@gmail.com>
2023-04-29 11:41:11 -0400

    fast-api connection

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,6 +3,7 @@ import requests
 
 
 
+# fastapi_url = "http://54.86.128.1:9000"
 fastapi_url = "http://54.86.128.1:9000"
 
 st.title("ChhaviAI")
@@ -10,7 +11,7 @@ st.title("ChhaviAI")
 username = st.text_input("Enter twitter username")
 if st.button("Generate Report"):
     response = requests.get(f"{fastapi_url}/brand-image-report/{username}")
-    st.write(response.json())
+    st.write(response.json()["response"])
 
 
 
@@ -19,4 +20,4 @@ query = st.text_input('Ask question about a specific product')
 if st.button("Ask"):
     json={"query": f"{query}"}
     response = requests.post(f"{fastapi_url}/vector-search/",json=json)
-    st.write(response)
+    st.write(response.json()["response"])
