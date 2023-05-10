e011db5c72b02e5157fd94ef2946eaf70f50f3ae
parvashah-create <jmparvashah@gmail.com>
2023-05-09 20:43:52 -0400

    streamlit app

--- fast_api.py
+++ fast_api.py
@@ -6,17 +6,17 @@ db_utils = DbUtils("tweet.db")
 
 
 
-def brand_image_report(username):
+def brand_image_report(username, openai_key):
     new_tweets = pipeline(username)
     if new_tweets:
         tweets  = db_utils.get_recent_sentiments("tweets",username)
         prompt  = create_prompt(tweets)
-        response  = generate_response(prompt)
+        response  = generate_response(prompt,openai_key)
     else:
         response = "Pipeline error!"    
     return {"response":response}
 
 
-def vector_search(string_input):
-    response = generative_search(string_input)
+def vector_search(string_input,openai_key):
+    response = generative_search(string_input,openai_key)
     return {"response":response}
